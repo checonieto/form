60077ffec40c744c5f7622f64a37a23ad91d1742
checonieto <checonietoh@gmail.com>
2024-12-09 22:16:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 // Product Array
 const products = [
-    { id: "prod1", name: "Product 1" },
-    { id: "prod2", name: "Product 2" },
-    { id: "prod3", name: "Product 3" },
-    { id: "prod4", name: "Product 4" },
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5,
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7,
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5,
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9,
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0,
+  },
 ];
 
 // Populate Product Name options
 const productSelect = document.getElementById("product");
 products.forEach((product) => {
-    const option = document.createElement("option");
-    option.value = product.id;
-    option.textContent = product.name;
-    productSelect.appendChild(option);
+  const option = document.createElement("option");
+  option.value = product.id; // Use the product ID as the value
+  option.textContent = `${product.name} (Rating: ${product.averagerating})`; // Display name with average rating
+  productSelect.appendChild(option);
 });
